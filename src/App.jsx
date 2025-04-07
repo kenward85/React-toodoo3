11703fb40ccbc806a8833e5c834cbe8006603fb3
Kenneth Ward Jr. <kenwardjr8592@gmail.com>
2025-04-07 01:06:44 -0400

    Lesson 1 revisions

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   const todos = [
     {id: 1, title: "review resources"},
     {id: 2, title: "take notes"},
-    {id: 3, title: "code out app"},
+    {id: 3, title: "code in app"},
   ]
   return (
     <div>
