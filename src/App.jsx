123d0f3bf9ed55646335a96930780a07555b8c8d
Kenneth Ward Jr. <kenwardjr8592@gmail.com>
2025-04-23 22:01:08 -0700

    Resolve merge conflict in App.jsx with sample todos

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
+import { useState } from 'react';
 import './App.css';
 import TodoForm from './TodoForm';
-import TodoList from './TodoLists'
+import TodoLists from './TodoLists';
 
 function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const handleAddTodo = (e) => {
+    e.preventDefault(); // prevent page reload
+    if (!newTodo.trim()) return;
+
+    const newTodoItem = {
+      id: Date.now(), // unique ID
+      title: newTodo,
+    };
+
+    setTodoList([...todoList, newTodoItem]); // add to list
+    setNewTodo(""); // clear input
+  };
+
   return (
     <div>
       <h1>Todo List</h1>
-      <TodoForm />
-      <TodoList />
+      <TodoForm 
+        newTodo={newTodo}
+        setNewTodo={setNewTodo}
+        handleAddTodo={handleAddTodo}
+      />
+      <p>{newTodo}</p>
+      <TodoLists todoList={todoList} />
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
